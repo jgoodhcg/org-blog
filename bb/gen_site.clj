47ff906591da56ecd8712d5e1e414f64d568ef76
Justin <jgoodhcg@gmail.com>
2023-04-22 16:36:06 -0400

    Switch to pandoc

--- bb/gen_site.clj
+++ bb/gen_site.clj
@@ -1,28 +1,19 @@
 (ns gen-site
-  (:require [babashka.curl :as curl]
-            [babashka.cli :as cli]
-            [clojure.term.colors :as c]
-            [puget.printer :as puget]
-            [babashka.process :as p]
-            [cljc.java-time.instant :as instant]))
+  (:require
+   [babashka.process :as p]
+   [clojure.term.colors :as c]))
 
 (defn ^:chatgpt org-to-html [org-file]
-  (let [cmd (str "emacs --batch --eval "
-                 "\"(progn"
-                 " (require 'ox-html)"
-                 " (with-current-buffer (find-file-noselect \\\"" org-file "\\\")"
-                 "   (org-html-export-as-html)"
-                 "   (princ (buffer-string))))\"")
-
+  (let [absolute-org-file (-> (java.io.File. org-file) (.getCanonicalFile))
+        cmd (str "pandoc -f org -t html5 " absolute-org-file)
         {:keys [exit out err]} (p/shell {:out :string} cmd)]
     (if (zero? exit)
       out
       (do (println "Error:" err)
           nil))))
 
-
 (defn -main []
   (-> "Parsing file.. " c/blue println)
-  (-> "./../posts/hello-world.org" org-to-html println)
+  (-> "./posts/hello-world.org" org-to-html println)
   (-> "Done!" c/blue println)
   )
