5695e84f4ea1adffe3ec4dc701945dd8978287d0
Justin <jgoodhcg@gmail.com>
2023-04-22 17:25:58 -0400

    Rest of the owl

--- src/gen_site.clj
+++ src/gen_site.clj
@@ -1,11 +1,13 @@
 (ns gen-site
   (:require
+   [clojure.java.io :as io]
    [clojure.java.shell :as shell]
    [clojure.term.colors :as c]
    [hiccup.core :refer [html]]
+   [hiccup.page :refer [include-css include-js]]
    [hickory.core :as hickory]))
 
-(defn ^:chatgpt org-to-html [org-file]
+(defn org-to-html [org-file]
   (let [absolute-org-file (-> (java.io.File. org-file) (.getCanonicalFile))
         cmd (str "pandoc -f org -t html5 " absolute-org-file)
         {:keys [exit out err]} (shell/sh "sh" "-c" cmd)]
@@ -17,17 +19,77 @@
 (defn wrap-in-hiccup [html-string]
   (let [parsed-html (hickory/parse-fragment html-string)
         hiccup-form (->> parsed-html (map hickory/as-hiccup))]
-    (html hiccup-form)))
+    (html [:div hiccup-form])))
 
-(defn -main []
-  (-> "Parsing file.. " c/blue println)
-  (->> "./posts/hello-world.org"
-       org-to-html
-       #_wrap-in-hiccup
-       println)
-  (-> "Done!" c/blue println)
-  (System/exit 0))
+(defn preamble [body]
+  [:div#preamble body])
+
+(defn postamble [body]
+  [:div#postamble body])
+
+(defn header [body]
+  [:html
+   [:head
+    [:meta {:charset "utf-8"}]
+    [:title "Generated Site"]
+    (include-css "/static/css/output.css")
+    #_(include-js "/js/scripts.js")]
+   [:body
+    [:header
+     [:nav]]
+    body
+    [:footer]]])
+
+(defn render-post [org-file out-dir]
+  (let [post (->> org-file
+                  org-to-html
+                  wrap-in-hiccup
+                  preamble
+                  postamble
+                  header
+                  html)
+        post-file-name (str (-> org-file
+                                (java.io.File.)
+                                (.getName)
+                                (clojure.string/replace #"\.org$" ".html")))
+        post-file-path (str out-dir "/" post-file-name)]
+    (spit post-file-path post)))
+
+(defn generate-index [org-files out-dir]
+  (let [latest-posts (map (fn [org-file]
+                             (let [post (->> org-file
+                                            org-to-html
+                                            wrap-in-hiccup
+                                            preamble
+                                            postamble)]
+                               [:div.post-preview post])) org-files)
+        index (-> [:div#index latest-posts] header html)]
+    (spit (str out-dir "/index.html") index)))
+
+(defn -main [& args]
+  (let [org-dir "./posts"
+        out-dir "./"]
+    (-> "Generating site..." c/blue println)
+    (-> out-dir io/file .mkdirs)
+    (let [org-files (->> org-dir
+                         io/file
+                         file-seq
+                         (filter #(re-matches #".*\.org" (.getName %)))
+                         (map #(.getCanonicalPath %)))]
+      (doseq [org-file org-files]
+        (render-post org-file out-dir))
+      (generate-index org-files out-dir))
+    (-> "Done!" c/blue println)
+    #_(System/exit 0)))
 
 (comment
+  (->> "./posts/hello-world.org"
+       org-to-html
+       wrap-in-hiccup
+       preamble
+       postamble
+       header
+       )
 
+  (-main)
   )
