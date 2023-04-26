0cb311f03ecd45f8cbdae09510af5d9a2824ecd2
Justin <jgoodhcg@gmail.com>
2023-04-25 20:58:25 -0400

    Add syntax highlighting

--- src/org_blog/core.clj
+++ src/org_blog/core.clj
@@ -1,5 +1,6 @@
 (ns org-blog.core
   (:require
+   [clojure.walk :as walk]
    [clojure.java.io :as io]
    [clojure.java.shell :as shell]
    [clojure.term.colors :as c]
@@ -9,25 +10,51 @@
 
 (defn org-to-html [org-file]
   (let [absolute-org-file (-> (java.io.File. org-file) (.getCanonicalFile))
-        cmd (str "pandoc -f org -t html5 " absolute-org-file)
+        cmd (str "pandoc -f org -t html5  " absolute-org-file)
         {:keys [exit out err]} (shell/sh "sh" "-c" cmd)]
     (if (zero? exit)
       out
       (do (println "Error:" err)
           nil))))
 
+(defn add-prism-class [hiccup]
+  (walk/postwalk
+   (fn [node]
+     (if (and (vector? node)
+              (= :code (first node)))
+       (let [lang       (->> (second node)
+                             :class
+                             (re-seq #"(?<=sourceCode\s)\w+")
+                             first)
+             lang-class (str "language-" lang)]
+         (-> node
+             (update 1 #(assoc % :class lang-class))))
+       node))
+   hiccup))
+
 (defn wrap-in-hiccup [html-string]
   (let [parsed-html (hickory/parse-fragment html-string)
-        hiccup-form (->> parsed-html (map hickory/as-hiccup))]
+        hiccup-form (->> parsed-html (map hickory/as-hiccup) add-prism-class)]
     (html [:div.content-container hiccup-form])))
 
 (defn post-header [body]
-  [:html.dark
+  [:html
    [:head
     [:meta {:charset "utf-8"}]
     [:title "Generated Site"]
     (include-css "../css/output.css")
-    #_(include-js "/js/scripts.js")]
+    #_(include-js "/js/scripts.js")
+    [:link {:rel            "stylesheet"
+            :href           "https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/themes/prism-okaidia.min.css"
+            :crossorigin    "anonymous"
+            :referrerpolicy "no-referrer"}]
+    [:script {:src            "https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/components/prism-core.min.js"
+              :crossorigin    "anonymous"
+              :referrerpolicy "no-referrer"}]
+    [:script {:src            "https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/plugins/autoloader/prism-autoloader.min.js"
+              :crossorigin    "anonymous"
+              :referrerpolicy "no-referrer"}]]
+
    [:body
     [:header
      [:nav]]
@@ -90,3 +117,5 @@
 
   (gen-index)
   )
+
+(-main)
