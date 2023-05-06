a6bb7483996ce7ae1abb1b4248d33ae8fcf98c18
Justin <jgoodhcg@gmail.com>
2023-05-06 19:55:19 -0400

    Isolate dev server

--- src/org_blog/core.clj
+++ src/org_blog/core.clj
@@ -2,16 +2,15 @@
   (:require
    [clojure.java.io :as io]
    [clojure.java.shell :as shell]
-   [clojure.string :refer [blank? split] :as string]
+   [clojure.string :refer [blank?] :as string]
    [clojure.term.colors :as c]
    [clojure.tools.namespace.repl :as ns-repl]
    [clojure.walk :as walk]
-   [hawk.core :as hawk]
    [hiccup.core :refer [html]]
    [hiccup.page :refer [include-css]]
    [hickory.core :as hickory]
-   [org-blog.pages.home :refer [gen-home]]
-   [org.httpkit.server :as http-kit])
+   [org-blog.dev-server :as dev-server]
+   [org-blog.pages.home :refer [gen-home]])
   )
 
 (defn org-to-html [org-file]
@@ -115,59 +114,24 @@
   (gen-home)
   (gen-posts))
 
-(def content-types
-  {"html" "text/html"
-   "css"  "text/css"
-   ;; Add other content types as needed
-   })
-
-(defn content-type-for [filename]
-  (let [ext (->> (split filename #"\.")
-                 last
-                 (get content-types))]
-    (or ext "text/plain")))
-
-(defn handler [req]
-  (let [resource-path (str "static" (:uri req))
-        file (io/file resource-path)]
-    (if (.exists file)
-      (if (.isDirectory file)
-        {:status  200
-         :headers {"Content-Type" "text/html"}
-         :body    (slurp (io/file (str resource-path "/index.html")))}
-        {:status  200
-         :headers {"Content-Type" (content-type-for resource-path)}
-         :body    (slurp file)})
-      {:status  404
-       :headers {"Content-Type" "text/plain"}
-       :body    "Not Found"})))
-
-(defn start-server []
-  (http-kit/run-server handler {:port 8080}))
-
-(defonce server-atom (atom nil))
-
-(when (nil? @server-atom)
+;; Start dev server
+(when (nil? @dev-server/server-atom)
   (-> "starting server" c/blue println)
   (regenerate-site)
-  (reset! server-atom (start-server)))
+  (reset! dev-server/server-atom (dev-server/start-server)))
 
-(defn watch-source-files [dirs]
-  (let [handler      (fn [ctx e]
-                       (when (= (:kind e) :modify)
-                         (println "File modified:" (:file e))
-                         ;; Calling `ns-repl/refresh` in another thread (hawk must run this handler in a another thread)
-                         ;; generates an error
-                         ;; By wrapping in future, by some magic, the function calls within are scheduled on the main thread
-                         (future
-                          (println "Refreshing repl ...")
-                          (ns-repl/refresh)
-                          (regenerate-site))))
-        watch-config (hawk/watch! [{:paths   dirs
-                                    :handler handler}])]
-    watch-config))
-
-(defonce source-watchers (atom nil))
-
-(when (nil? @source-watchers)
-  (reset! source-watchers (watch-source-files ["src"])))
+;; Start file watcher
+(when (nil? @dev-server/source-watchers)
+  (reset! dev-server/source-watchers
+          (dev-server/watch-source-files
+           ["src"]
+           (fn [ctx e]
+             (when (= (:kind e) :modify)
+               (println "File modified:" (:file e))
+               ;; Calling `ns-repl/refresh` in another thread (hawk must run this handler in a another thread)
+               ;; generates an error
+               ;; By wrapping in future, by some magic, the function calls within are scheduled on the main thread
+               (future
+                 (println "Refreshing repl ...")
+                 (ns-repl/refresh)
+                 (regenerate-site)))))))
---
+++ src/org_blog/dev_server.clj
@@ -0,0 +1,45 @@
+(ns org-blog.dev-server
+  (:require
+   [clojure.java.io :as io]
+   [clojure.string :refer [split] :as string]
+   [hawk.core :as hawk]
+   [org.httpkit.server :as http-kit]))
+
+(def content-types
+  {"html" "text/html"
+   "css"  "text/css"
+   "js"   "application/javascript"
+   ;; Add other content types as needed
+   })
+
+(defn content-type-for [filename]
+  (let [ext (->> (split filename #"\.")
+                 last
+                 (get content-types))]
+    (or ext "text/plain")))
+
+(defn handler [req]
+  (let [resource-path (str "static" (:uri req))
+        file (io/file resource-path)]
+    (if (.exists file)
+      (if (.isDirectory file)
+        {:status  200
+         :headers {"Content-Type" "text/html"}
+         :body    (slurp (io/file (str resource-path "/index.html")))}
+        {:status  200
+         :headers {"Content-Type" (content-type-for resource-path)}
+         :body    (slurp file)})
+      {:status  404
+       :headers {"Content-Type" "text/plain"}
+       :body    "Not Found"})))
+
+(defn start-server []
+  (http-kit/run-server handler {:port 8080}))
+
+(defonce server-atom (atom nil))
+
+(defn watch-source-files [dirs handler]
+  (hawk/watch! [{:paths   dirs
+                 :handler handler}]))
+
+(defonce source-watchers (atom nil))
