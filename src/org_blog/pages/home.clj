77ed1f91c6897cfe65b3f9c60dd9641dfd3a32ef
Justin <jgoodhcg@gmail.com>
2023-05-06 15:52:16 -0400

    Add stub best practices to home page

--- src/org_blog/pages/home.clj
+++ src/org_blog/pages/home.clj
@@ -6,26 +6,36 @@
 
 (defn gen-home []
   (-> "Generating home (index) page" c/blue println)
-  (-> [:html.dark
+  (-> [:html {:lang "en"} ; Add language attribute
        [:head
         [:meta {:charset "utf-8"}]
         [:meta {:name "viewport" :content "width=device-width, initial-scale=1.0"}]
+        [:meta {:name "description" ; Add a description meta tag
+                :content "Justin Good's personal blog about software engineering, permaculture, and more."}]
+        ; Add Open Graph tags
+        [:meta {:property "og:title" :content "Jgood Blog"}]
+        [:meta {:property "og:description" :content "Justin Good's personal blog about software engineering, permaculture, and more."}]
+        [:meta {:property "og:type" :content "website"}]
+        [:meta {:property "og:url" :content "https://your-domain.com"}] ;; TODO
+        [:meta {:property "og:image" :content "https://your-domain.com/path/to/your/image.jpg"}] ;; TODO
+
         [:title "Jgood Blog"]
         (include-css "./css/output.css")
-        #_(include-js "/js/scripts.js")]
+        [:link {:rel "icon" :href "/path/to/favicon.ico"}] ;; TODO
+        ]
        [:body
         [:header
          [:nav]]
-        [:div.content-container
-         [:h1 "What's my h1?"]
-         [:p (str "I'm Justin Good. "
-                  "As a full stack software engineer, I have a diverse range of interests and skills. "
+        [:main.content-container ; Use semantic HTML tags
+         [:h1 "Welcome to Jgood Blog"]
+         [:p (str "I'm Justin Good, a full stack software engineer with a wide range of interests and skills. "
                   "My professional experience mainly revolves around web applications "
                   "and mobile development using React Native on Expo, with a focus on writing Clojure code. "
                   "When I step away from the keyboard, "
                   "I enjoy transforming my lawn into a food forest and a biodiversity haven, doodling cartoons, "
                   "delving into sci-fi novels, and immersing myself in ancient history and anthropology through audio-books. "
-                  "I care about addressing the climate crisis, promoting socialism, advocating for abolition, and advancing longevity research. Welcome to my website, where I share my work, interests, and the values. "
+                  "I care about addressing the climate crisis, promoting socialism, advocating for abolition, and advancing longevity research. "
+                  "Welcome to my website, where I share my work, interests, and values. "
                   )]]
         [:footer]]]
       html
