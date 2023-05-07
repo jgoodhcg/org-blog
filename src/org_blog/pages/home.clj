99cffd58379d2ff1478ff4d4326782af4d22e1c0
Justin <jgoodhcg@gmail.com>
2023-05-07 14:40:08 -0400

    Add side panel hover effects

--- src/org_blog/pages/home.clj
+++ src/org_blog/pages/home.clj
@@ -10,7 +10,7 @@
        [:head
         [:meta {:charset "utf-8"}]
         [:meta {:name "viewport" :content "width=device-width, initial-scale=1.0"}]
-        [:meta {:name "description" ; Add a description meta tag
+        [:meta {:name    "description" ; Add a description meta tag
                 :content "Justin Good's personal blog about software engineering, permaculture, and more."}]
         ; Add Open Graph tags
         [:meta {:property "og:title" :content "Jgood Blog"}]
@@ -40,10 +40,9 @@
                     "Welcome to my website, where I share my work, interests, and values. "
                     )]]]
          [:div.pt-1.mt-2.w-full.rounded-tl-3xl.bg-purple-900.flex.flex-row.lcars-bottom-border
-          [:div.py-4.w-8.md:w-48
-           [:div.px-4.hidden.md:block.border-t-2.border-black "side panel content here"]
-           [:div.px-4.hidden.md:block.border-t-2.border-black "side panel content here"]
-           [:div.px-4.hidden.md:block.border-t-2.border-black "side panel content here"]]
+          [:div.py-4.w-8.md:w-48.bg-gradient-to-b.hover:from-purple-900.hover:to-purple-900.hover:via-purple
+           (for [i (-> (range 25))]
+             [:div.px-4.hidden.md:block.border-t-2.border-black.hover:bg-purple-100 "side panel content here"])]
           [:div.p-4.w-full.rounded-tl-lg.bg-black
            [:h2.text-purple "Whatever"]
            [:a {:href "posts/hello-world"} "hello world post"]
